9107e360a68246f6cddf27f852cdfcf46436cedb
EREN SARIIŞIK <erensariisik03@gmail.com>
2026-09-07 12:13:26 +0300

    yaptık

--- app/src/main/java/com/eren/gumustakip/MainActivity.java
+++ app/src/main/java/com/eren/gumustakip/MainActivity.java
@@ -2,6 +2,7 @@ package com.eren.gumustakip;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.*;
 import android.content.pm.PackageManager;
 import android.graphics.*;
